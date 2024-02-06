3ae1bc559ec3fa5a75db2a6a43464d3dec7b3b9a
Emanuel Acosta <e.acosta1695@gmail.com>
2024-02-06 10:32:31 -0300

    Added a spinner to indicate that audio is being transcribed to text.

--- Frontend/src/components/ShowSelectedFile.jsx
+++ Frontend/src/components/ShowSelectedFile.jsx
@@ -8,13 +8,17 @@ export const ShowSelectedFile = ({ clearFileAndNotification, audioFile, setAudio
     const [language, setLanguage] = useState('');
     const [text, setText] = useState(null);
     const [editableText, setEditableText] = useState('');
+    const [isLoading, setIsLoading] = useState(false)
 
     const handleConvert = async () => {
+        setIsLoading(true);
+
         try {
             const { data } = await fetchAudioToText(audioFile, language);
             console.log('Texto convertido:', data);
             setText(data.texto_transcrito);
             setEditableText(data.texto_transcrito); // Inicializa el texto editable con el texto convertido
+            setIsLoading(false);
         } catch (error) {
             console.error('Error al convertir audio a texto:', error);
         }
@@ -30,30 +34,36 @@ export const ShowSelectedFile = ({ clearFileAndNotification, audioFile, setAudio
 
     return (
         <div className="drop-zone">
-            {!text ? (
-                <>
-                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'white' }}>
-                        <p style={{ marginBottom: '5px', textDecoration: 'underline' }}>Archivo de audio seleccionado:</p>
-                        <p style={{ marginBottom: '10px' }}>{audioFile.name}</p>
-                        <DeleteSelectedFileButton clearFileAndNotification={clearFileAndNotification} />
-                    </div>
-                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'white', marginTop: '5%' }}>
-                        <p style={{ marginBottom: '10px' }}>¿En qué idioma está el archivo?</p>
-                        <Dropdown onLanguageChange={handleLanguageChange} />
-                    </div>
-                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'white', marginTop: '6%' }}>
-                        <p style={{ marginBottom: '3%' }}>¿Desea transcribir el audio seleccionado?</p>
-                        <button className='btn btn-success' onClick={handleConvert} disabled={!language}>Convertir</button>
-                    </div>
-                </>
-            ) : (
-                <Textarea 
-                    editableText={editableText} 
-                    handleTextChange={handleTextChange}
-                    setText={setText}
-                    setAudioFile={setAudioFile}
-                />
-            )}
+            {isLoading ? 
+                <div className="spinner-border text-light" style={{width: '3rem', height: '3rem'}} role="status">
+                    <span class="sr-only"></span>
+                </div>
+                : 
+                !text ? (
+                    <>
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'white' }}>
+                            <p style={{ marginBottom: '5px', textDecoration: 'underline' }}>Archivo de audio seleccionado:</p>
+                            <p style={{ marginBottom: '10px' }}>{audioFile.name}</p>
+                            <DeleteSelectedFileButton clearFileAndNotification={clearFileAndNotification} />
+                        </div>
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'white', marginTop: '5%' }}>
+                            <p style={{ marginBottom: '10px' }}>¿En qué idioma está el archivo?</p>
+                            <Dropdown onLanguageChange={handleLanguageChange} />
+                        </div>
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'white', marginTop: '6%' }}>
+                            <p style={{ marginBottom: '3%' }}>¿Desea transcribir el audio seleccionado?</p>
+                            <button className='btn btn-success' onClick={handleConvert} disabled={!language}>Convertir</button>
+                        </div>
+                    </>
+                ) : (
+                    <Textarea 
+                        editableText={editableText} 
+                        handleTextChange={handleTextChange}
+                        setText={setText}
+                        setAudioFile={setAudioFile}
+                    />
+                    )
+            }
         </div>
     );
 };
